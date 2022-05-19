e43a4147ed8848263278e5021def44f4e06ba3ea
Felipe Curty <felipecrp@gmail.com>
2022-05-18 23:17:04 -0300

    Add public folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // fetch("http://localhost:8080/user");
+
   return (
     <div className="App">
       <header className="App-header">
